77db42a9b8212bb20feec5e4bafe105d65d951ad
Dert1129 <nathancreger1111@gmail.com>
2023-08-08 11:33:07 -0400

    add tests for add methods

--- src/test/java/org/kpmp/repositoryDataset/RepositoryDatasetDisplayTest.java
+++ src/test/java/org/kpmp/repositoryDataset/RepositoryDatasetDisplayTest.java
@@ -32,10 +32,32 @@ public class RepositoryDatasetDisplayTest {
   }
 
   @Test
+  public void testAddRedCapId() {
+    assertEquals(0, repositoryDatasetDisplay.getRedcapId().size());
+
+    RepositoryDatasetDisplay repositoryDatasetDisplay = new RepositoryDatasetDisplay(repositoryFile);
+    repositoryDatasetDisplay.addRedCapId("redcap_id");
+
+    assertEquals(1, repositoryDatasetDisplay.getRedcapId().size());
+    assertEquals(repositoryDatasetDisplay, repositoryDatasetDisplay.getRedcapId());
+  }
+
+  @Test
   public void setRedcapId(){
     Set<String> expected = new HashSet<String>(Arrays.asList("redcap_id", "redcap_id2"));
     Set<String> actual = repositoryDatasetDisplay.getRedcapId();
     assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testAddSampleType(){
+    assertEquals(0, repositoryDatasetDisplay.getSampleType().size());
+
+    RepositoryDatasetDisplay repositoryDatasetDisplay = new RepositoryDatasetDisplay(repositoryFile);
+    repositoryDatasetDisplay.addSampleType("sample_type");
+
+    assertEquals(1, repositoryDatasetDisplay.getSampleType().size());
+    assertEquals(repositoryDatasetDisplay, repositoryDatasetDisplay.getSampleType());
   }
 
   @Test
@@ -46,10 +68,32 @@ public class RepositoryDatasetDisplayTest {
   }
 
   @Test
+  public void testAddTissueType() {
+    assertEquals(0, repositoryDatasetDisplay.getTissueType().size());
+
+    RepositoryDatasetDisplay repositoryDatasetDisplay = new RepositoryDatasetDisplay(repositoryFile);
+    repositoryDatasetDisplay.addTissueType("tissue_type");
+
+    assertEquals(1, repositoryDatasetDisplay.getTissueType().size());
+    assertEquals(repositoryDatasetDisplay, repositoryDatasetDisplay.getTissueType());
+  }
+
+  @Test
   public void setTissueType() {
     Set<String> expected = new HashSet<String>(Arrays.asList("tissue_type"));
     Set<String> actual = repositoryDatasetDisplay.getTissueType();
     assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testAddAgeBinned() {
+    assertEquals(0, repositoryDatasetDisplay.getAgeBinned().size());
+
+    RepositoryDatasetDisplay repositoryDatasetDisplay = new RepositoryDatasetDisplay(repositoryFile);
+    repositoryDatasetDisplay.addAgeBinned("40-49");
+
+    assertEquals(1, repositoryDatasetDisplay.getAgeBinned().size());
+    assertEquals(repositoryDatasetDisplay, repositoryDatasetDisplay.getAgeBinned());
   }
 
   @Test
@@ -60,10 +104,32 @@ public class RepositoryDatasetDisplayTest {
   }
 
   @Test
+  public void testAddSex() {
+    assertEquals(0, repositoryDatasetDisplay.getSex().size());
+
+    RepositoryDatasetDisplay repositoryDatasetDisplay = new RepositoryDatasetDisplay(repositoryFile);
+    repositoryDatasetDisplay.addSex("Female");
+
+    assertEquals(1, repositoryDatasetDisplay.getSex().size());
+    assertEquals(repositoryDatasetDisplay, repositoryDatasetDisplay.getSex());
+  }
+
+  @Test
   public void setSex() {
     Set<String> expected = new HashSet<String>(Arrays.asList("male"));
     Set<String> actual = repositoryDatasetDisplay.getSex();
     assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testAddDoi() {
+    assertEquals(0, repositoryDatasetDisplay.getDoi().size());
+
+    RepositoryDatasetDisplay repositoryDatasetDisplay = new RepositoryDatasetDisplay(repositoryFile);
+    repositoryDatasetDisplay.addDoi("doi");
+
+    assertEquals(1, repositoryDatasetDisplay.getDoi().size());
+    assertEquals(repositoryDatasetDisplay, repositoryDatasetDisplay.getDoi());
   }
 
   @Test
@@ -92,10 +158,32 @@ public class RepositoryDatasetDisplayTest {
   }
 
   @Test
+  public void testAddProtocol() {
+    assertEquals(0, repositoryDatasetDisplay.getProtocol().size());
+
+    RepositoryDatasetDisplay repositoryDatasetDisplay = new RepositoryDatasetDisplay(repositoryFile);
+    repositoryDatasetDisplay.addProtocol("protocol");
+
+    assertEquals(1, repositoryDatasetDisplay.getProtocol().size());
+    assertEquals(repositoryDatasetDisplay, repositoryDatasetDisplay.getProtocol());
+  }
+
+  @Test
   public void setProtocol() {
     Set<String> expected = new HashSet<String>(Arrays.asList("Protocol"));
     Set<String> actual = repositoryDatasetDisplay.getProtocol();
     assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testAddTissueSource() {
+    assertEquals(0, repositoryDatasetDisplay.getTissueSource().size());
+
+    RepositoryDatasetDisplay repositoryDatasetDisplay = new RepositoryDatasetDisplay(repositoryFile);
+    repositoryDatasetDisplay.addTissueSource("tissue_source");
+
+    assertEquals(1, repositoryDatasetDisplay.getTissueSource().size());
+    assertEquals(repositoryDatasetDisplay, repositoryDatasetDisplay.getTissueSource());
   }
 
   @Test
@@ -106,10 +194,32 @@ public class RepositoryDatasetDisplayTest {
   }
 
   @Test
+  public void testAddExperimentalStrategy() {
+    assertEquals(0, repositoryDatasetDisplay.getExperimentalStrategy().size());
+
+    RepositoryDatasetDisplay repositoryDatasetDisplay = new RepositoryDatasetDisplay(repositoryFile);
+    repositoryDatasetDisplay.addExperimentalStrategy("experimental_strategy");
+
+    assertEquals(1, repositoryDatasetDisplay.getExperimentalStrategy().size());
+    assertEquals(repositoryDatasetDisplay, repositoryDatasetDisplay.getExperimentalStrategy());
+  }
+
+  @Test
   public void setExperimentalStrategy() {
     Set<String> expected = new HashSet<String>(Arrays.asList("experimental strategy"));
     Set<String> actual = repositoryDatasetDisplay.getExperimentalStrategy();
     assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testAddWorkflowType() {
+    assertEquals(0, repositoryDatasetDisplay.getWorkflowType().size());
+
+    RepositoryDatasetDisplay repositoryDatasetDisplay = new RepositoryDatasetDisplay(repositoryFile);
+    repositoryDatasetDisplay.addWorkflowType("workflow_type");
+    
+    assertEquals(1, repositoryDatasetDisplay.getWorkflowType().size());
+    assertEquals(repositoryDatasetDisplay, repositoryDatasetDisplay.getWorkflowType());
   }
 
   @Test
