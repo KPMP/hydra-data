197b75f3c9f9d1b26b217fc865bc369da0baa37a
Dert1129 <nathancreger1111@gmail.com>
2023-08-08 10:30:04 -0400

    cleanup queries

--- src/main/java/org/kpmp/repositoryDataset/RepositoryFileDatasetRepository.java
+++ src/main/java/org/kpmp/repositoryDataset/RepositoryFileDatasetRepository.java
@@ -13,9 +13,6 @@ public interface RepositoryFileDatasetRepository
 
     List<RepositoryFileDataset> findAll();
 
-    @Query (value = "SELECT MAX(release_version) AS max FROM ar_file_info", nativeQuery = true)
+    @Query (value = "SELECT MAX(release_version) AS max FROM repo_file_v", nativeQuery = true)
     public String max();
-
-    @Query (value = "SELECT release_version FROM ar_file_info", nativeQuery = true)
-    public String getReleaseVersion();
 }
